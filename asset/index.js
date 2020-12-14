d6a74d5c8b4ecadb9b24d21b36f28fbc900d5c1c
hilmi2207 <hilmionam@gmail.com>
2020-12-14 21:11:51 +0700

    initial commit

--- asset/index.js
+++ asset/index.js
@@ -1,21 +1,8 @@
-window.onscroll = function () {
-    scrollFunction();
-  };
-  
-  function scrollFunction() {
-    if (
-      document.body.scrollTop > 150 ||
-      document.documentElement.scrollTop > 150
-    ) {
-      document.getElementById("navbar").style.backgroundColor = "white";
-    } else {
-      var x = document.getElementById("navbar");
-      x.style.background = "black";
-      document.getElementById("title").style.color = "red";
-      document.getElementById("nav-name1").style.color = "red";
-      document.getElementById("nav-name2").style.color = "red";
-      document.getElementById("nav-name3").style.color = "red";
-      document.getElementById("nav-name4").style.color = "red";
-    }
+function myFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
-  
+}
